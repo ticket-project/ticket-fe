9040198eda06e56c3ef7c5a2a370416d4c0e2c96
heewon <rlagmldnjs88@gmail.com>
2026-02-24 18:57:52 +0900

    feat: /main 접속 시 /main/concert로 리다이렉트

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,20 +1,7 @@
-import { Suspense } from 'react';
-
-import { Container, Skeleton } from '@mui/material';
+import { redirect } from 'next/navigation';
 
 const Homepage = () => {
-  return (
-    <div>
-      메인페이지 준비중입니다.
-      {/* <Suspense fallback={<Skeleton height={400} />}>Banner!</Suspense>
-      <Container maxWidth="lg" sx={{ mb: 4, mt: 4 }}>
-        <Suspense fallback={<Skeleton height={300} />}>UpcomingShows!</Suspense>
-        <Suspense fallback={<Skeleton height={800} />}>
-          Concerts Section!
-        </Suspense>
-      </Container> */}
-    </div>
-  );
+  redirect('/main/concert');
 };
 
 export default Homepage;
